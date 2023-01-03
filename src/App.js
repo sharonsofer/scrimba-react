676faccd35ad6c95b6fedf257882d0340148da8e
Sharon Sofer <sharon.sofer@gmail.com>
2023-01-03 20:26:29 +0200

    Initial load

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Xnavbar from './components/Xnavbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Xnavbar />
+    </div>
+  );
+}
+
+export default App;
