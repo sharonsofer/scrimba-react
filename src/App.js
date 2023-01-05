13289cde5c9ad88cbdc83c4a108a54fe2f88525b
Sharon Sofer <sharon.sofer@gmail.com>
2023-01-05 17:38:32 +0200

    Latest updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import MainContent from "./components/MainContent";
+// import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <Header />
+  //     <MainContent />
+  //     <Footer />
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
+    <div className="container">
+      <Navbar />
+      <Main />
     </div>
   );
 }
